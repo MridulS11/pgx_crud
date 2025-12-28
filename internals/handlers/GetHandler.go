594ca8887d518965d5822a561a898ae15b17a444
Mridul Sharma <m@mmac.local>
2025-12-28 19:52:45 +0530

    Pagination Added

--- internals/handlers/GetHandler.go
+++ internals/handlers/GetHandler.go
@@ -2,15 +2,39 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"pg_crud/internals/models"
+	"strconv"
 )
 
 func(c * ConnPool) GetHandler(w http.ResponseWriter, r *http.Request){
 
-	query := "SELECT id, title, author, category FROM books"
+	limitstr := r.URL.Query().Get("limit")
+	offsetstr := r.URL.Query().Get("offset")
 
-	rows, err := c.Db.Query(r.Context(), query)
+	limit := 10
+	if l, err := strconv.Atoi(limitstr); err == nil && l > 0{
+		limit = l
+	}
+
+	offset := 0
+	if o, err := strconv.Atoi(offsetstr); err == nil && o > 0{
+		offset = o
+	}
+
+	var total int
+	err := c.Db.QueryRow(r.Context(), "SELECT COUNT(*) FROM books").Scan(&total)
+
+	if total <= limit{
+		fmt.Fprintf(w, "Viewed All Results: %d\n", total)
+	}else{
+		fmt.Fprintf(w, "Viewed %d Results out of %d.\n", limit, total)
+	}
+
+	query := "SELECT id, title, author, category FROM books Order BY id LIMIT $1 OFFSET $2"	//Select itself is returning
+
+	rows, err := c.Db.Query(r.Context(), query, limit, offset)
 	if err != nil{
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
