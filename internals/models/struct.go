2ab8fe0f0e150f4c61fc24338ff7389868746b61
Mridul Sharma <m@mmac.local>
2026-01-03 14:32:10 +0530

    Category Added

--- internals/models/struct.go
+++ internals/models/struct.go
@@ -4,5 +4,5 @@ type BookData struct{
 	Id 		int `json:"id"`
 	Title 	string `json:"title" validate:"required,min=3,max=100,forbidden"`
 	Author 	string `json:"author" validate:"required"`
-	Category string `json:"category" validate:"required,oneofci=Tech Fantasy Misc. Programming"`
+	Category string `json:"category" validate:"required,oneofci=Tech Fantasy Misc. Programming Fiction"`
 }
